f7809b9347b1f2784b607d9e6182fcd2767be5fe
Amaraben <amarachi.benikenador@dcmail.ca>
2025-02-07 21:24:58 -0500

    final commit
    Row span removed for some cells and errors cleared

---
+++ Lab 1/Part 1/public class Main {.java
@@ -0,0 +1,57 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+     
+        
+     
+
+
+
+monthTemps[0] = new float [31];
+monthTemps[1] = new float [28];
+monthTemps[2] = new float [31];
+
+//temps are between -20c and 30c
+monthTemps[0][0] = (float) ((50.0 * Math.random())-20.0);
+
+System.out.println(monthTemps[0][0]);
+
+
+for (int iMonth = 0; imonth< month.lenght; imonth ++) {
+		for (int iDay = 0; iDay < monthTemps[month].length; iDay++)
+				monthTemps[iMonth][iDay] = (float) ((50.0 * math.random())-20.0);
+}
+
+for (int iMonth = 0; imonth< month.lenght; imonth ++) {
+		String temperature = ""
+		
+Int tempTotal =  0;
+For (int i =0; i < dailyTemps[i];)
+    System.out.println("Day " + i + " " + dailyTemps[i]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+    }
+}

dc9bcaaf537bdc05a464c4abedcaea9baf376df4
Amaraben <amarachi.benikenador@dcmail.ca>
2025-04-04 22:15:52 -0400

    Final Commit
    The final changes are made. the variable names are corrected from "var" to "const" and the placeholder texts are properly replaced

--- lab4/part1/javascript/main.js
+++ lab4/part1/javascript/main.js
@@ -20,40 +20,42 @@ function randomValueFromArray(array){
 }
 
 
-var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
-",
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-const insertX = ["Willy the Goblin",
-			  "Big Daddy",
-			  "Father Christmas"];
-const insertY = ["the soup kitchen",
-			  "Disneyland",
-			  "the White House"];
-const insertZ = ["spontaneously combusted",
-			   "melted into a puddle on the sidewalk",
-			   "turned into a slug and crawled away"];
+const insertX = ["Willy the Goblin","Big Daddy","Father Christmas"];
+const insertY = ["the soup kitchen","Disneyland","the White House"];
+const insertZ = ["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and crawled away"];
 			   
 			   
 randomize.addEventListener('click', result);
 
 function result() {
+	let newStory = storyText;
 
+	const xItem = randomValueFromArray(insertX);
+	const yItem = randomValueFromArray(insertY);
+	const zItem = randomValueFromArray(insertZ);
+
+	// This will replace the placeholder values in the story.
+	newStory = newStory.replace(":insertx:", xItem);
+	newStory = newStory.replace(":inserty:", yItem);
+	newStory = newStory.replace(":insertz:", zItem);
+
+// replace Bob if a new name is added
   if(customName.value !== '') {
     const name = customName.value;
+	newStory = newStory.replace("Bob", name);
   }
 
-var newStory = storyText
-var xItem = randomValueFromArray(insertX);
-var yItem = randomValueFromArray(insertY);
-var zItem = randomValueFromArray(insertZ);
 
-
+// US to UK value conversions
 if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-	}
-	story.textContent = "";
-	story.style.visibility = 'visible';
+    const weight = Math.round(300/14)+" stone";
+	const temperature = Math.round((94-32)*5/9)+" centigrade";
+	newStory = newStory.replace("94 fahrenheit", temperature);
+	newStory = newStory.replace("300 pounds", weight);
 }
 
-
+	story.textContent = newStory;
+	story.style.visibility = 'visible';
+}

fea99a606af2108624011f7e84851541ef378c4d
Amaraben <amarachi.benikenador@dcmail.ca>
2025-04-07 00:53:57 -0400

    Final commit
    The last parts of the code is updated making the balls change color when they collide

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -24,7 +24,6 @@ function randomRGB() {
 }
 
 
-
 class Ball {
   constructor(x, y, velX, velY, color, size) {
     this.x = x;
@@ -33,29 +32,8 @@ class Ball {
     this.velY = velY;
     this.color = color;
     this.size = size;
-
-    update() {
-      if ((this.x + this.size) >= width) {
-        this.velX = -(this.velX);
-      }
-    
-      if ((this.x - this.size) <= 0) {
-        this.velX = -(this.velX);
-      }
-    
-      if ((this.y + this.size) >= height) {
-        this.velY = -(this.velY);
-      }
-    
-      if ((this.y - this.size) <= 0) {
-        this.velY = -(this.velY);
-      }
-    
-      this.x += this.velX;
-      this.y += this.velY;
-    }
-    
   }
+
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
@@ -65,4 +43,70 @@ class Ball {
   
 
 
+  update() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+    
+    if ((this.x - this.size) <= 0) {
+       this.velX = -(this.velX);
+    }
+    
+    if ((this.y + this.size) >= height) {
+       this.velY = -(this.velY);
+    }
+    
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+    
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
 }
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),  
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size,
+  );
+
+  balls.push(ball);
+}
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
+  
+
